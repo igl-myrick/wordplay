57c3f977e74cbcfc4473894e89b5d632a6c2b60a
igl-myrick <igl.myrick@gmail.com>
2023-11-30 15:44:00 -0800

    add functionality for clearing previous results

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,17 @@ window.addEventListener("load", function() {
     const inputArray = Array.from(formInput.split(" "));
     const outputArray = inputArray.reverse().filter((word) => word.length > 2);
 
-    let h1 = document.createElement("h1");
+    const h1 = document.createElement("h1");
     h1.append("Result");
     document.body.append(h1);
 
-    let p = document.createElement("p");
+    const p = document.createElement("p");
     p.append(outputArray.join(" "));
     document.body.append(p);
+
+    form.addEventListener("submit", function() {
+      h1.remove();
+      p.remove();
+    });
   });
 });
